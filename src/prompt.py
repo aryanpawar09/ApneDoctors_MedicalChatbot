0a4d372ddf8c6d0b9be082db9a4c05d468fd1263
aryanpawar09 <aryanpawar2805@gmail.com>
2025-04-15 11:10:13 +0530

    Update prompt.py

--- src/prompt.py
+++ src/prompt.py
@@ -1,5 +1,4 @@
-
-prompt_template="""
+prompt_template = """
 Use the following pieces of information to answer the user's question.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
@@ -7,5 +6,6 @@ Context: {context}
 Question: {question}
 
 Only return the helpful answer below and nothing else.
+If you detect any type of query has a specific language like Hindi, Marathi, you should be able to answer it in that particular language.
 Helpful answer:
 """
